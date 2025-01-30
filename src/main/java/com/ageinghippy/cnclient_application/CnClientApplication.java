c195eb6a3242ef07f4982e47a93bbb7adb84a13f
AgeingHippy <charles_mcdonald@hotmail.com>
2025-01-30 21:52:59 +0000

    Initial commit

---
+++ src/main/java/com/ageinghippy/cnclient_application/CnClientApplication.java
@@ -0,0 +1,13 @@
+package com.ageinghippy.cnclient_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ageinghippy/cnclient_application/CnClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ageinghippy.cnclient_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
